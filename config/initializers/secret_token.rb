2147aeffa01ed7c59c2ae593a7f11639d0e8f31e
incheon (U24A - Win8) <Bonriki.Life@gmail.com>
2014-03-16 20:57:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EMondaiBeta::Application.config.secret_key_base = 'df6d037cb684533efdfb6c905cc1814335c91f657127256d42b4de01350eee236baad314aa522f5d11a171172d799756fc468a6eba91e68a4b17b5b24810927e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EMondaiBeta::Application.config.session_store :cookie_store, key: '_e-mondai-beta_session'
