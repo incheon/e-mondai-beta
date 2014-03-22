d3b7c9e7412ec2a331c32cb907c488685ff840b5
incheon (U24A - Win8) <Bonriki.Life@gmail.com>
2014-03-23 00:12:02 +0900

    add about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ EMondaiBeta::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/welcome"
   get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
 
 end
