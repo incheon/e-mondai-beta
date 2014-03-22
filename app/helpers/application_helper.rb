3a9a6ff52f2c30d36b630c492935b89d1de01a12
incheon (U24A - Win8) <Bonriki.Life@gmail.com>
2014-03-22 22:23:25 +0900

    newly define helper method: full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # サイトのタイトルにページタイトルをつなげたものを返す
+  def full_title(page_title)
+    base_title = "自分だけの問題を作って公開 e-Mondai"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
