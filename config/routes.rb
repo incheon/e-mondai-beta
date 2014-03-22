fcc184ae127dd110aa236ddd9fb213f774f4133d
incheon (U24A - Win8) <Bonriki.Life@gmail.com>
2014-03-23 00:35:44 +0900

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 EMondaiBeta::Application.routes.draw do
 
-  get "users/new"
+  # root
   root "static_pages#home"
   
+  # Static Pages
   match 'about',   to: "static_pages#about",   via: 'get'
   match 'contact', to: "static_pages#contact", via: 'get'
   match 'help',    to: "static_pages#help",    via: 'get'
   match 'welcome', to: "static_pages#welcome", via: 'get'
+  
+  # Users
+  get "users/new"
+  match 'signup',  to: "users#new", via: 'get'
 
 end
