a58032ae24ca36723689dfb1ab469ac847536a6b
incheon (vmware centos) <bonriki.life@gmail.com>
2014-03-31 22:38:00 +0900

    set test for static page content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'e-Mondai beta'" do
-      visit '/static_pages/home'
-      page.should have_content('e-Mondai beta')
+    it "should have the content 'Home'" do
+      visit '/'
+      page.should have_content('Home')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/about'
+      page.should have_content('About')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/help'
+      page.should have_content('Help')
+    end
+  end
+
 end
