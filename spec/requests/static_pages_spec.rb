4f078423e2cce4fbb4dc00ac9d27938fc75a93f0
incheon (vmware centos) <bonriki.life@gmail.com>
2014-04-01 11:27:36 +0900

    set test for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,38 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-    it "should have the content 'Home'" do
+    it "should have the h1 'e-Mondai'" do
       visit '/'
-      page.should have_content('Home')
+      page.should have_selector('h1', :text => 'e-Mondai beta')
     end
-  end
 
-  describe "About page" do
-    it "should have the content 'About'" do
-      visit '/about'
-      page.should have_content('About')
+    it "should have the title 'Home'" do
+      visit '/'
+      page.should have_selector('title', :text => "eMondai beta | Home")
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/help'
+      page.should have_selector('title', :text => "eMondai beta | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the h1 'About'" do
+      visit '/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/about'
+      page.should have_selector('title', :text => "eMondai beta | About")
     end
   end
 
