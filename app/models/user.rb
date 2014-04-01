92c60e22bfe6e5fdd86347a6267212c3c8c5f978
incheon (vmware centos) <bonriki.life@gmail.com>
2014-04-01 15:50:58 +0900

    set case sensitive false to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { within: 4..20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
 end
