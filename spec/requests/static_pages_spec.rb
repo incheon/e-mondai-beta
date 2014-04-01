33db827eb142091abdda81dfa8c16604550fe698
incheon (vmware centos) <bonriki.life@gmail.com>
2014-04-01 13:24:52 +0900

    summarized all static page test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,35 +14,20 @@ describe "Static pages" do
 
   describe "Help page" do
     before {visit help_path}
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      page.should have_selector('title', :text => "| Help")
-    end
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => "| Help") }
   end
 
   describe "About page" do
     before {visit about_path}
-    it "should have the h1 'About'" do
-      page.should have_selector('h1', :text => 'About')
-    end
-
-    it "should have the title 'About'" do
-      page.should have_selector('title', :text => "| About")
-    end
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_selector('title', :text => "| About") }
   end
 
   describe "Contact page" do
     before {visit contact_path}
-    it "should have the content 'Contact'" do
-      page.should have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      page.should have_selector('title', :text => "| Contact")
-    end
+    it { page.should have_content('Contact') }
+    it { page.should have_selector('title', :text => "| Contact") }
   end
 
 end
